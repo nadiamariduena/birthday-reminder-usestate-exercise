801d91c706f1a6944244b76ae8be2904e7fe6e9d
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-10 22:29:35 +0100

    setting up the components

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ## [Birthday Reminder](https://youtu.be/a_7Z7C_JCyo)
 
-##### 7:20
+- Check the [ link | 7:20 ](https://youtu.be/a_7Z7C_JCyo) to see all the projects
+
+<br>
+
+
+##### credits images:
+
+- [Cottonbro](https://www.pexels.com/@cottonbro/collections/)
 
 <br>
 <br>
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: 1,
+    name: "Sam Yales",
+    age: 29,
+    image:
+      "https://images.pexels.com/photos/10675990/pexels-photo-10675990.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    id: 2,
+    name: "Henry Yates",
+    age: 30,
+    image:
+      "https://images.pexels.com/photos/10675991/pexels-photo-10675991.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  },
+  {
+    id: 3,
+    name: "Paul Hatch",
+    age: 31,
+    image:
+      "https://images.pexels.com/photos/10675992/pexels-photo-10675992.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  },
+  {
+    id: 4,
+    name: "Alice Pohl",
+    age: 31,
+    image:
+      "https://images.pexels.com/photos/8343923/pexels-photo-8343923.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  },
+];
